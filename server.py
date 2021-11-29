e852bbbd28f7a0dff2296ce5304543d407f81620
syncmas <65772720+petrovskydv@users.noreply.github.com>
2021-11-29 16:22:50 +0300

    add prototype of a streaming site
    Took 1 minute

--- server.py
+++ server.py
@@ -1,9 +1,34 @@
-from aiohttp import web
+import asyncio
+import datetime
+
 import aiofiles
+from aiohttp import web
+
+INTERVAL_SECS = 1
 
 
 async def archivate(request):
     raise NotImplementedError
+
+
+async def uptime_handler(request):
+    response = web.StreamResponse()
+
+    # Большинство браузеров не отрисовывают частично загруженный контент, только если это не HTML.
+    # Поэтому отправляем клиенту именно HTML, указываем это в Content-Type.
+    response.headers['Content-Type'] = 'text/html'
+
+    # Отправляет клиенту HTTP заголовки
+    await response.prepare(request)
+
+    while True:
+        formatted_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        message = f'{formatted_date}<br>'  # <br> — HTML тег переноса строки
+
+        # Отправляет клиенту очередную порцию ответа
+        await response.write(message.encode('utf-8'))
+
+        await asyncio.sleep(INTERVAL_SECS)
 
 
 async def handle_index_page(request):
@@ -16,6 +41,7 @@ if __name__ == '__main__':
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
+        web.get('/archive/7kna/', uptime_handler),
         web.get('/archive/{archive_hash}/', archivate),
     ])
     web.run_app(app)
