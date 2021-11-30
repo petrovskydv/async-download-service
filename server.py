3a356db5f78e04edce562378e02be01c0293bd1b
syncmas <65772720+petrovskydv@users.noreply.github.com>
2021-11-30 13:16:15 +0300

    add settings from environment variables
    Took 42 minutes

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import argparse
 import asyncio
 import datetime
 import logging
@@ -5,19 +6,20 @@ import os.path
 from asyncio import CancelledError
 
 import aiofiles
-import aiohttp.web
 from aiohttp import web
 
 INTERVAL_SECS = 1
+DELAY = 0
+PHOTO_PATH = ''
 
 logger = logging.getLogger('marathon-bot')
 
 
 async def archivate(request):
     archive_hash = request.match_info.get('archive_hash')
-    directory_path = os.path.join('test_photos', archive_hash)
+    directory_path = os.path.join(PHOTO_PATH, archive_hash)
     if not os.path.exists(directory_path):
-        raise aiohttp.web.HTTPNotFound(reason='Архив не существует или был удален')
+        raise web.HTTPNotFound(reason='Архив не существует или был удален')
 
     response = web.StreamResponse()
     response.headers['Content-Disposition'] = f'attachment; filename="{archive_hash}.zip"'
@@ -32,7 +34,7 @@ async def archivate(request):
             data = await zip_process.stdout.read(512 * 1024)
             logger.debug('Sending archive chunk ...')
             await response.write(data)
-            await asyncio.sleep(1)
+            await asyncio.sleep(DELAY)
     except (CancelledError, BaseException):
         logger.debug('Download was interrupted')
         raise
@@ -73,15 +75,33 @@ async def handle_index_page(request):
     return web.Response(text=index_contents, content_type='text/html')
 
 
-if __name__ == '__main__':
+def main():
+    parser = argparse.ArgumentParser(
+        description='Service for download archives'
+    )
+    parser.add_argument('-l', '--loglevel', default=os.getenv('LOGGING_LEVEL', 'ERROR'),
+                        help='logging level')
+    parser.add_argument('-d', '--delay', type=int, default=os.getenv('RESPONSE_DELAY', 0),
+                        help='response delay for chunks of zip archive')
+    parser.add_argument('-p', '--path', default=os.getenv('FILE_STORAGE_PATH', 'test_photos'),
+                        help='path to catalog with photos')
+
+    global DELAY, PHOTO_PATH
+    args = parser.parse_args()
+    DELAY = args.delay
+    PHOTO_PATH = args.path
+
     logging.basicConfig(
-        level='DEBUG',
+        level=args.loglevel,
         format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
     )
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
-        # web.get('/archive/7kna/', uptime_handler),
         web.get('/archive/{archive_hash}/', archivate),
     ])
     web.run_app(app)
+
+
+if __name__ == '__main__':
+    main()
